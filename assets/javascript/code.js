ed71a0b5d04340a2ab6ee80e71fc9c250f8390fb
SiennaS <ssienna48@gmail.com>
2018-10-20 13:38:38 -0400

    almost done

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -4,19 +4,46 @@ $(document).ready(function () {
 
     event.preventDefault();
 
-    var giph = $("giphy-input").val();
+    var giph = $("#giphy-input").val();
 
-    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=m49dp38ck8IOmA26XZqQ7kr8Fw6plpoF";
+      console.log(giph);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giph + "&api_key=m49dp38ck8IOmA26XZqQ7kr8Fw6plpoF&limit=10";
   
+    console.log(queryURL);
+
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
+      for(i = 0; i < response.data.length; i++){
+        var imgUrl = response.data[i].images.fixed_height.url;
+        var div = document.getElementById("giphy-view")
+        var x = document.createElement("IMG");
+        x.setAttribute("src", imgUrl);
+        x.setAttribute("width", "304");
+        x.setAttribute("height", "228");
+        div.appendChild(x);
+      }
+
        console.log(response);
-      $("#giphy-view").text(JSON.stringify(response));
+
     });
 
-    $(giph).append("giphy-input")
-  
+
   })
+
+  $(imgUrl).on("click", function(event) {
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+    console.log(response);
+  }
+  )
+
 });
